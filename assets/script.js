c79460e03fe3eabc35d7a6f6d8b2ef913e1d47ce
JohnPac <johnpac15@yahoo.com>
2021-11-30 11:34:36 -0600

    added function for current date at the top of web page

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+var currentDate = document.getElementById("currentDay");
+
+
+theDate = function() {
+    let todaysDate = new Date();
+    currentDate.innerHTML = todaysDate;
+    
+
+}
+
+theDate();
