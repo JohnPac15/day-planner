11cc209f780fdc6b69b140dbf21c016e5fbba263
JohnPac <johnpac15@yahoo.com>
2021-12-01 14:21:51 -0600

    added texrarea and save function

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,43 @@
 var currentDate = document.getElementById("currentDay");
+var timeSlot = document.getElementById("time-slot")
+
+var form = document.getElementById("form")
+var task = document.getElementById("textarea")
+
+// console.log(time)
+
+var hour = moment().set({'hour': 9, 'minute': 00, 'seconds': 00})
+// var minute = moment().set('minute', '0')
+
+// var task = [];
+// var taskDate = $("#modalDueDate").val();
 
 
-theDate = function() {
-    let todaysDate = new Date();
-    currentDate.innerHTML = todaysDate;
+let todaysDate = new Date();
+console.log(todaysDate);
+currentDate.innerHTML = todaysDate;
+
+
+var showTime = timeSlot.innerHTML = hour
+var blockTime =showTime._d
+console.log(blockTime)
+
+
+saveTask = function() {
     
-
+       
 }
 
-theDate();
+var auditDate = function(taskEl) {
+    var date = $(taskEl)
+    .find("span")
+    .text()
+    .trim();
+
+    console.log(taskEl)
+}
+auditDate();
+
+
+// let time = todaysDate.getHours()
+// console.log(time)
