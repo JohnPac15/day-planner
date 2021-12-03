eba097a4fb544b31e44da47374a26323fd8a3d5f
JohnPac <johnpac15@yahoo.com>
2021-12-02 22:46:53 -0600

    added button values

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,92 @@
 var currentDate = document.getElementById("currentDay");
-var timeSlot = document.getElementById("time-slot")
+// var timeSlot = document.getElementById("time-slot")
+var Nine = document.getElementById("9am")
 
 var form = document.getElementById("form")
 var task = document.getElementById("textarea")
 
-// console.log(time)
+savedData = JSON.parse(localStorage.getItem("data"));
 
-var hour = moment().set({'hour': 9, 'minute': 00, 'seconds': 00})
+
+console.log(Nine)
+
+// var hour = moment().set({'hour': 9, 'minute': 00, 'seconds': 00})
 // var minute = moment().set('minute', '0')
 
 // var task = [];
 // var taskDate = $("#modalDueDate").val();
-
+// console.log(timeSlot).name
 
 let todaysDate = new Date();
 console.log(todaysDate);
+
+let getHour = todaysDate.getHours();
+console.log(getHour);
 currentDate.innerHTML = todaysDate;
 
 
-var showTime = timeSlot.innerHTML = hour
-var blockTime =showTime._d
-console.log(blockTime)
+// var showTime = timeSlot.innerHTML = hour
+// var blockTime =showTime._d
+// console.log(blockTime)
 
 
-saveTask = function() {
+$(".savebtn").click(function(){
+    var todo = $("#textarea").val();
+    btnNumber = $(this).val();
+    console.log(btnNumber)
     
-       
+    var taskList = {
+    
+        toDoText: todo,
+        button: btnNumber,
+    
+    };
+
+    console.log(taskList)
+
+    if(savedData == null) {
+        console.log(savedData)
+        savedData = []
+        savedData.push(taskList)
+        localStorage.setItem("data", JSON.stringify(todo))
+        alert("Item Saved")
+    }else {
+        console.log(savedData)
+        console.log(todo)
+        savedData.push(taskList)
+        localStorage.setItem("data", JSON.stringify(todo))
+        alert("Item Saved")
+
+    }
+
+})
+
+for(i =0; i < 9; i++){
+    var thisTimeBlock = i + 9
+    console.log(thisTimeBlock)
+    var thishour = ("#") + thisTimeBlock
+    console.log(thishour)
+
+    if(thisTimeBlock < getHour){
+        $(thishour).addClass("bg-primary")
+    }
+
+    if(thisTimeBlock == getHour) {
+        $(thishour).addClass("bg-warning text-dark")
+    }
+
+    if(thisTimeBlock > getHour){
+        $(thishour).addClass("bg-secondary")
+    }
+    else{
+        $(thishour).addClass("bg-danger")
+    }
+
+
+}
+
+displayLocalStorage = function() {
+    savedData
 }
 
 var auditDate = function(taskEl) {
