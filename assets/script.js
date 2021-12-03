1989a4bb0811e1b0e80c5c0ada60f8fe5f27bff1
JohnPac <johnpac15@yahoo.com>
2021-12-03 11:52:15 -0600

    local storge is now working

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,23 @@
+var task = {};
 var currentDate = document.getElementById("currentDay");
 // var timeSlot = document.getElementById("time-slot")
 var Nine = document.getElementById("9")
-var div = document.querySelector(".todo")
+var divMain = document.querySelector(".todo")
 
+var textBox = document.querySelector(".textBox")
 
+console.log(textBox)
 var form = document.getElementById("form")
 var task = document.getElementById("textarea")
 
-savedData = JSON.parse(localStorage.getItem("data"));
+arrayData = JSON.parse(localStorage.getItem("data"));
+console.log(arrayData)
 
 
 
 // var hour = moment().set({'hour': 9, 'minute': 00, 'seconds': 00})
 // var minute = moment().set('minute', '0')
 
-// var task = [];
 // var taskDate = $("#modalDueDate").val();
 // console.log(timeSlot).name
 // var showTime = timeSlot.innerHTML = hour
@@ -28,42 +31,73 @@ let getHour = todaysDate.getHours();
 console.log(getHour);
 currentDate.innerHTML = todaysDate;
 
+$(".addbtn").on("click", function() {
+    console.log("clicked")
+    let div2El = document.createElement("div")
+    let formEl = document.createElement("form")
+    let inputEl = document.createElement("input")
+    let selectEl = document.createElement("select")
+    let newOption1 = new Option('9:00 Am', 9)
+    let newOption2 = new Option('10:00 Am', 10)
+    let newOption3 = new Option('11:00 Am',11)
+    let newOption4 = new Option('12:00 Pm',12)
+    let optionEl = document.createElement("options")
 
+    
 
-$(".savebtn").click(function(){
-    var todo = $("#textarea").val();
-    btnNumber = $(this).val();
-    console.log(btnNumber)
     
-    var taskList = {
+    $(divMain).addClass("h-100")
+    $(div2El).addClass("border-dark h-100")
+    $(formEl).addClass(" textBox border-white w-100 h-100")
+    $(selectEl).addClass(" border-white")
+    $(inputEl).addClass("taskItem w-100 border-dark")
+    inputEl.setAttribute("style", "width: 100%; border: 1px solid white");
+    
+    divMain.appendChild(div2El)
+    div2El.appendChild(formEl)
+    formEl.appendChild(inputEl)
+    formEl.appendChild(selectEl)
+    selectEl.appendChild(newOption1)
+    selectEl.appendChild(newOption2)
+    selectEl.appendChild(newOption3)
+    selectEl.appendChild(newOption4)
+
+    
+    $(".savebtn").click(function(event){
+        event.preventDefault();
+    
+        todoTask = document.querySelector(".taskItem");
+        taskText = todoTask.value;
+        optionNumber = selectEl.value
+        // timeBlock = btnNumber * 1
+        console.log(todoTask.value)
+        console.log(optionNumber)
         
-        toDoText: todo,
-        button: btnNumber,
+        var taskList = 
+        {
+            task: taskText,
+            button: optionNumber,   
+        };
+        console.log(taskList)
         
-    };
-    var saveData = function() {
-        localStorage.setItem("data", JSON.stringify(todo))
-    }
-    
-    
-    console.log(taskList)
-    
-    if(savedData == null) {
-        console.log(savedData)
-        savedData = []
-        savedData.push(taskList)
-        saveData();
-        alert("Item Saved")
-    }else {
-        console.log(savedData)
-        console.log(todo)
-        savedData.push(taskList)
-        saveData();
-        alert("Item Saved")
+        if(arrayData == null) {
+            console.log(arrayData)
+            arrayData = []
+            arrayData.push(taskList)
+            localStorage.setItem("data", JSON.stringify(arrayData));
+            alert("First Item Saved")
+        }else {
+            console.log(arrayData)
+            arrayData.push(taskList)
+            localStorage.setItem("data", JSON.stringify(arrayData));
+            alert("Item Saved")
+            
+        };
         
-    };
-    
+    })
 })
+
+
 
 for(i =0; i < 9; i++){
     var thisTimeBlock = i + 9
@@ -89,29 +123,14 @@ for(i =0; i < 9; i++){
     
 }
 
-$(".addbtn").on("click", function() {
-    console.log("clicked")
-    let li = document.createElement("li")
-    let text = document.createElement("textarea")
-    
-    li.setAttribute("style", "width: 100%;");
-    text.setAttribute("style", "width: 100%; border: 1px solid white");
-    
-    
-    
-    div.appendChild(li)
-    li.appendChild(text)
-    
-    let divNumber = $("this")
-    console.log(divNumber)
-})
 
 
-displayLocalStorage = function() {
-    savedData = JSON.parse(localStorage.getItem("data"));
-    console.log(savedData)
-}
+// displayLocalStorage = function() {
+//     debugger
+//     savedData
+//     console.log(savedData)
+// }
 
-displayLocalStorage();
+// displayLocalStorage();
 
 
