60bb08b62c6bae8c5909331b216a5f370c31ebfd
JohnPac <johnpac15@yahoo.com>
2021-12-04 00:23:46 -0600

    made changes so there are multi btn and options are hidden now

--- assets/script.js
+++ assets/script.js
@@ -2,19 +2,28 @@ var task = {};
 var currentDate = document.getElementById("currentDay");
 var divMain = document.querySelector(".todo")
 var textBox = document.querySelector(".textBox")
-var spanText = document.querySelector(".todo-form")
-var addbtn = document.getElementById("add")
-var btn9 =document.querySelector("addbtn-9")
-var btn10 =document.querySelector("addbtn-10")
-var btn11 =document.querySelector("addbtn-11")
-var btn12 =document.querySelector("addbtn-12")
-var btn13 =document.querySelector("addbtn-13")
-var btn14 =document.querySelector("addbtn-14")
-var btn15 =document.querySelector("addbtn-15")
-var btn16 =document.querySelector("addbtn-16")
-var btn17 =document.querySelector("addbtn-17")
 
-console.log(textBox)
+var spanText9 = document.querySelector(".todo-form-9")
+var spanText10 = document.querySelector(".todo-form-10")
+var spanText11 = document.querySelector(".todo-form-11")
+var spanText12 = document.querySelector(".todo-form-12")
+var spanText13 = document.querySelector(".todo-form-13")
+var spanText14 = document.querySelector(".todo-form-14")
+var spanText15 = document.querySelector(".todo-form-15")
+var spanText16 = document.querySelector(".todo-form-16")
+var spanText17 = document.querySelector(".todo-form-17")
+
+var addbtn = document.getElementById("add")
+var btn9 =document.querySelector(".addbtn-9")
+var btn10 =document.querySelector(".addbtn-10")
+var btn11 =document.querySelector(".addbtn-11")
+var btn12 =document.querySelector(".addbtn-12")
+var btn13 =document.querySelector(".addbtn-13")
+var btn14 =document.querySelector(".addbtn-14")
+var btn15 =document.querySelector(".addbtn-15")
+var btn16 =document.querySelector(".addbtn-16")
+var btn17 =document.querySelector(".addbtn-17")
+
 var form = document.getElementById("form")
 var task = document.getElementById("textarea")
 
@@ -29,96 +38,82 @@ var timeBlock3 = document.getElementById("15")
 var timeBlock4 = document.getElementById("16")
 var timeBlock5 = document.getElementById("17")
 
+let newOption1 = new Option('9:00 Am', 9)
+let newOption2 = new Option('10:00 Am', 10)
+let newOption3 = new Option('11:00 Am',11)
+let newOption4 = new Option('12:00 Pm',12)
+let newOption5 = new Option('1:00 Pm',13)
+let newOption6 = new Option('2:00 Pm',14)
+let newOption7 = new Option('3:00 Pm',15)
+let newOption8 = new Option('4:00 Pm',16)
+let newOption9 = new Option('5:00 Pm',17)
+
 arrayData = JSON.parse(localStorage.getItem("data"));
 console.log(arrayData)
 
-divArray= [
-    timeBlock9,
-    timeBlock10,
-    timeBlock11,
-    timeBlock12,
-    timeBlock1,
-    timeBlock2,
-    timeBlock3,
-    timeBlock4,
-    timeBlock5
-];
-divndex = 0
-clickTheDiv = function() {
-    console.log(divArray[0].text)
-
-
-}
-
-
 var hour = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
 var timeHour = moment().hour()
-var timeSpecial = moment().format("h:mm:ss a")
-console.log(timeSpecial)
-
-let todaysDate = new Date();
-console.log(todaysDate);
-
-let getHour = todaysDate.getHours();
-console.log(getHour);
-
-
+var timeSpecial = moment().format("MMMM Do")
 currentDate.innerHTML = hour;
 
 reload = function () {
     document.location.reload();
 }
 
-
-
 btn9.addEventListener("click", function() {
-    btn9 = 9 
+    createTextfeild(spanText9, newOption1);   
+})
+btn10.addEventListener("click", function() {
+    createTextfeild(spanText10, newOption2);   
+})
+btn11.addEventListener("click", function() {
+    createTextfeild(spanText11, newOption3);   
+})
+btn12.addEventListener("click", function() {
+    createTextfeild(spanText12, newOption4);   
+})
+btn13.addEventListener("click", function() {
+    createTextfeild(spanText13, newOption5);   
+})
+btn14.addEventListener("click", function() {
+    createTextfeild(spanText14, newOption6);   
+})
+btn15.addEventListener("click", function() {
+    createTextfeild(spanText15, newOption7);   
+})
+btn16.addEventListener("click", function() {
+    createTextfeild(spanText16, newOption8);   
+})
+btn17.addEventListener("click", function() {
+    createTextfeild(spanText17, newOption9);   
 })
 
+createTextfeild = function(x, y) {
 
-
-$("#add").on("click", function(event) {
-    console.log(event.target, "click event")
+    console.log(x, y, "click event")
     let div2El = document.createElement("div")
     let formEl = document.createElement("form")
     let inputEl = document.createElement("input")
     let selectEl = document.createElement("select")
-    let newOption1 = new Option('9:00 Am', 9)
-    let newOption2 = new Option('10:00 Am', 10)
-    let newOption3 = new Option('11:00 Am',11)
-    let newOption4 = new Option('12:00 Pm',12)
-    let newOption5 = new Option('1:00 Pm',13)
-    let newOption6 = new Option('2:00 Pm',14)
-    let newOption7 = new Option('3:00 Pm',15)
-    let newOption8 = new Option('4:00 Pm',16)
-    let newOption9 = new Option('5:00 Pm',17)
-
-    
-
-    
+  
     $(divMain).addClass("h-100")
     $(div2El).addClass("border-dark h-100")
     $(formEl).addClass(" textBox border-white w-100 h-100")
-    $(selectEl).addClass(" border-white")
+    $(selectEl).addClass("border-white")
+    selectEl.setAttribute("style", "display: none;")
     $(inputEl).addClass("taskItem w-100 border-dark")
     inputEl.setAttribute("style", "width: 100%; border: 1px solid white");
+    li.setAttribute("style", "padding: 7px;")
     
-    spanText.appendChild(div2El)
+    x.appendChild(div2El)
     div2El.appendChild(formEl)
     formEl.appendChild(inputEl)
     formEl.appendChild(selectEl)
-    selectEl.appendChild(newOption1)
-    selectEl.appendChild(newOption2)
-    selectEl.appendChild(newOption3)
-    selectEl.appendChild(newOption4)
-    selectEl.appendChild(newOption5)
-    selectEl.appendChild(newOption6)
-    selectEl.appendChild(newOption7)
-    selectEl.appendChild(newOption8)
-    selectEl.appendChild(newOption9)
-
+    selectEl.appendChild(y)
     
+
     $(".savebtn").click(function(event){
+
         event.preventDefault();
     
         todoTask = document.querySelector(".taskItem");
@@ -141,21 +136,17 @@ $("#add").on("click", function(event) {
             arrayData = []
             arrayData.push(taskList)
             localStorage.setItem("data", JSON.stringify(arrayData));
-            alert("First Item Saved")
-            spanText.textContent = ""
             reload();
         }else {
             console.log(arrayData)
             arrayData.push(taskList)
             localStorage.setItem("data", JSON.stringify(arrayData));
-            alert("Item Saved")
-            spanText.textContent = ""
             reload();
             
         };
     });
     
-})
+}
 
 displayTask = function(){
     if(arrayData === null) {
@@ -199,7 +190,7 @@ displayTask = function(){
             timeBlock3.appendChild(li)
         }
         if(numberSlot == 16){
-            li.textContent = arrayText+ " :Saved at " + arrrayTime
+            li.textContent = arrayText+ " / " + arrrayTime
             timeBlock4.appendChild(li)
         }
         if(numberSlot == 17){
@@ -212,12 +203,11 @@ displayTask = function(){
 
 for(i =0; i < 9; i++){
     var thisTimeBlock = i + 9
-    // console.log(thisTimeBlock)
     var thishour = ("#") + thisTimeBlock
     console.log(timeHour)
     
     if(thisTimeBlock < timeHour){
-        $(thishour).addClass("bg-danger")
+        $(thishour).addClass("bg-danger text-white")
     }
     
     if(thisTimeBlock == timeHour) {
